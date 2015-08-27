8a06254a09e5161340dbf1d414e67d991f249c2e
Nick Bolt <nick@yoomee.com>
2015-08-27 13:20:45 +0100

    add upcoming meals to calendar view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,14 +10,15 @@ class ApplicationController < ActionController::Base
   end
 
   def dashboard
-    @meals_json = current_user
-      .meals
+    meals = current_user.meals
+    @meals_json = meals
       .collect{ |meal| {
       title: meal.recipe.name,
       start: meal.date.strftime("%Y-%m-%d"),
       url: edit_meal_url(meal),
       color: Meal.time_colour(meal.time)
     } }.to_json
+    @upcoming_meals = meals.select{ |meal| meal.date >= Date.today }
   end
 
 end
