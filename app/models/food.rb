09bca3489a902b3f40065c315361fbb550d14af0
Nick Bolt <nick@yoomee.com>
2015-08-27 13:21:55 +0100

    more aisles

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,16 +1,20 @@
 class Food < ActiveRecord::Base
   belongs_to :user
+  has_many :ingredients, dependent: :destroy
+  has_many :recipes, through: :ingredients
 
   validates :name, presence: true, uniqueness: { scope: :user_id }
   validates :unit_name, presence: true
 
   attr_accessor :recipe_id
 
+  default_scope { order(:name) }
+
   def name_unit
     "#{name} (#{unit_name})"
   end
 
   def self.aisle_options
-    %w( fruit vegatables fresh meat cooked_meat seafood canned_goods cupboard_basics fridge treats household_goods )
+    %w( fruit vegatables dairy fresh_meat cooked_meat seafood canned_goods cupboard_basics fridge bakery cereal treats household_goods ).sort
   end
 end
